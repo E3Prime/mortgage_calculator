5d53ccc6bb79131f4bc477622f5b76f5e2fb3461
E3Prime <primesoftware3@gmail.com>
2026-01-09 13:00:51 -0700

    quick functionality setup still working on the logic and rendering results

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 initialize();
 
-function initialize() {}
+function initialize() {
+  const mortgageCalculatorElem = /** @type {HTMLFormElement} */ (document.getElementById('mortgageCalculator'));
+  const mortgageResultsElem = /** @type {HTMLElement} */ (document.getElementById('mortgageResults'));
+  const clearInfoBtn = /** @type {HTMLButtonElement} */ (mortgageCalculatorElem.querySelector('#clearInfo'));
+  const dynamicCommasRegex = /\B(?=(\d{3})+(?!\d))/g;
+  const numsOnlyRegex = /[^\d]/g;
+
+  /** @param {Event} e */
+  const formatMortgage = (e) => {
+    const input = /** @type {HTMLInputElement} */ (e.target);
+    const inputId = input.id;
+    if (inputId === 'mortgageAmount') input.value = input.value.replace(numsOnlyRegex, '').replace(dynamicCommasRegex, ',');
+    else if (inputId === 'mortgageTerm') input.value = input.value.replace(numsOnlyRegex, '');
+  };
+
+  /** @param {SubmitEvent} e */
+  const calculateMortgage = (e) => {
+    e.preventDefault();
+    const form = /** @type {HTMLFormElement} */ (e.target);
+    const {mortgageAmount, mortgageTerm, interestRate, repaymentChoice, interestChoice} = /** @type {Object.<string, HTMLInputElement>} */ (form);
+  };
+
+  clearInfoBtn.addEventListener('click', () => mortgageCalculatorElem.reset());
+  mortgageCalculatorElem.addEventListener('input', formatMortgage);
+  mortgageCalculatorElem.addEventListener('submit', calculateMortgage);
+}
